fc7c6b2ac98d679e94ca2d6df3f0938ae9a92ee4
Tal Ledeniov <tal.ledeniov@gmail.com>
2022-06-10 17:34:09 -0400

    kinda works locally :shrug:

--- add.js
+++ add.js
@@ -1,8 +1,26 @@
 var button = document.getElementById("btn");
-var postNameCounter = 0;
+
+if (localStorage.getItem('editIndex')!=null) {
+    // potentially an issue - editIndex may store as a string so if this doesn't work thats prob why
+    var postList = JSON.parse(localStorage.getItem("prevItems"));
+    var index = JSON.parse(localStorage.getItem('editIndex'));
+    localStorage.removeItem('editIndex');
+    var postData = postList[index];
+    console.log(postData);
+    document.querySelector('#name').value = postData.title;
+    document.querySelector('#tags').value = postData.tags;
+    document.querySelector('#num1').value = postData.items[0];
+    document.querySelector('#num2').value = postData.items[1];
+    document.querySelector('#num3').value = postData.items[2];
+    document.querySelector('#num4').value = postData.items[3];
+    document.querySelector('#num5').value = postData.items[4];
+}
+
 button.addEventListener("click", function () {
-    // it says elements
-    document.getElementsByClassName("display-1")[0].innerHTML = "Hello World";
+
+    // add a check here to make sure none of the fields are empty
+    // we could do something as simple as alert()
+
     var name = document.getElementById("name").value;
     var tags = document.getElementById("tags").value.split(",");
     var items = [];
--- index.js
+++ index.js
@@ -3,16 +3,15 @@ var postList = JSON.parse(localStorage.getItem("prevItems"));
 console.log(postList);
 
 if (postList != null) {
-    for(var i=0; i<postList.length; i++){
+    for(var i=postList.length-1; i>=0; i--){
         console.log(i);
-        makeNewPost(postList[i])
+        makeNewPost(postList[i], i)
     }
     
 }
 
-function makeNewPost(postData) {
+function makeNewPost(postData, index) {
     // later: add id to these elements for CSS purposes
-    console.log("entered makeNewPost: "+ postData);
     const element = document.createElement("li");
     element.classList.add('list-group-item');
     element.classList.add('my-5');
@@ -22,10 +21,23 @@ function makeNewPost(postData) {
     div1.style.width = '18rem';
     element.appendChild(div1);
 
-    const div2 = document.createElement("div");
-    div2.classList.add('card-header');
-    div2.innerHTML = "Top 5 " + postData.title;
-    div1.appendChild(div2);
+    const postHeader = document.createElement("div");
+    postHeader.classList.add('card-header');
+
+    const postTitle = document.createElement("div");
+
+    postTitle.innerHTML = ("Top 5 " + postData.title).bold();
+    postTitle.style.fontSize = '25px'
+    postHeader.appendChild(postTitle);
+
+    div1.appendChild(postHeader);
+
+    const tagsHolder = document.createElement("div");
+    tagsHolder.classList.add('card-subtitle');
+    tagsHolder.classList.add('mb-2');
+    tagsHolder.classList.add('text-muted');
+    tagsHolder.innerHTML = "Tags: " + postData.tags;
+    postHeader.appendChild(tagsHolder);
 
     const top5list = document.createElement("ul");
     top5list.classList.add('list-group');
@@ -38,7 +50,19 @@ function makeNewPost(postData) {
         item.innerHTML = (i+1) + ". " + postData.items[i];
         top5list.appendChild(item);
     }
-    
+
+    const anchor = document.createElement("a");
+    anchor.href = "add.html";
+    div1.appendChild(anchor);
+
+    const editPostButton = document.createElement("button");
+    editPostButton.classList.add("edit");
+    editPostButton.innerHTML="Edit";
+    editPostButton.addEventListener("click", ()=> {
+        // call a function with post at index "index" in add.js
+        localStorage.setItem('editIndex', index);
+    })
+    anchor.appendChild(editPostButton);
 
     document.querySelector('ul').appendChild(element);
 }
